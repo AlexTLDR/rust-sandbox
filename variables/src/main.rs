aa3b210452c2b8e1ff1b32851bab09d3a701862b
AlexTLDR <alex.badragan@protonmail.com>
2025-11-30 17:22:28 +0100

    style(main): clean up variable declarations and formatting

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,10 +1,11 @@
-const STARTING_MISSILES :i32 = 8;
-const READY_AMOUNT :i32 = 2;
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
 
 fn main() {
-    let mut missiles = STARTING_MISSILES;
-    let ready = READY_AMOUNT;
-    missiles = missiles - ready;
+    // let mut missiles = STARTING_MISSILES;
+    // let ready = READY_AMOUNT;
+    let (missiles, ready) = (STARTING_MISSILES, READY_AMOUNT); //no need for mutable variables if we are not changing the value of missiles
+    // missiles = missiles - ready;
     println!("Firing {} of my {} missiles...", ready, missiles);
-    println!("{} missiles left", missiles);
+    println!("{} missiles left", missiles - ready);
 }
