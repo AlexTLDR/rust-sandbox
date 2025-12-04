738c54d2656e3a198d26018e17a6172cace2cbe4
AlexTLDR <alex.badragan@protonmail.com>
2025-12-04 20:07:14 +0100

    style(main): format code for better readability
    * Adjust indentation for consistency
    * Improve readability of input reading function

--- foundations/session1/variables/src/main.rs
+++ foundations/session1/variables/src/main.rs
@@ -1,11 +1,12 @@
 fn main() {
-   let input = read_line();
+    let input = read_line();
     println!("You typed: [{}]", input);
 }
 
-
 fn read_line() -> String {
     let mut input = String::new();
-    std::io::stdin().read_line(&mut input).expect("Stdin not working");
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Stdin not working");
     input.trim().to_string()
 }
