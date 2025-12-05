287d21ce80753a2e0222211ff8b92064262894a8
AlexTLDR <alex.badragan@protonmail.com>
2025-12-05 11:56:18 +0100

    feat(main): add authentication and login packages
    * Introduced `authentication` package with a greeting function.
    * Created `login` package dependent on `authentication`.
    * Updated workspace members in Cargo.toml to include new packages.

---
+++ foundations/session1/authentication/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn greet_user(name: &str) -> String {
+    format!("Hello {}", name)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_greet_user() {
+        assert_eq!("Hello Alex", greet_user("Alex"));
+    }
+
+}
