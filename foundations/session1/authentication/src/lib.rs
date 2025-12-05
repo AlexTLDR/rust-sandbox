502fdad63e145194b2ff49ef00a229711f115b95
AlexTLDR <alex.badragan@protonmail.com>
2025-12-05 17:42:52 +0100

    feat(auth): refactor login function and add user struct
    * Introduce User struct to encapsulate user data
    * Implement get_users function to manage user instances
    * Refactor login function to utilize user data for authentication
    * Enhance password checking and role retrieval logic

--- foundations/session1/authentication/src/lib.rs
+++ foundations/session1/authentication/src/lib.rs
@@ -15,26 +15,45 @@ pub enum LoginAction {
     Granted(LoginRole),
     Denied,
 }
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub enum LoginRole {
     Admin,
     User,
 }
 
+pub struct User {
+    pub username: String,
+    pub password: String,
+    pub role: LoginRole,
+}
+
+impl User {
+    pub fn new(username: &str, password: &str, role: LoginRole ) -> User {
+        User {
+            username: username.to_lowercase(),
+            password: password.to_string(),
+            role,
+        }
+    }
+}
+
+pub fn get_users() -> [User; 2] {
+    [
+        User::new("admin", "password", LoginRole::Admin),
+        User::new("bob", "password", LoginRole::User)
+    ]
+}
 pub fn login(username: &str, password: &str) -> Option<LoginAction> {
     let username = username.to_lowercase();
-
-    if username != "admin" && username != "bob" {
-        return None;
+    let users= get_users();
+    if let Some(user) = users.iter().find(|user|user.username == username) {
+        if user.password == password {
+            return Some(LoginAction::Granted(user.role.clone()));
+        } else {
+            return Some(LoginAction::Denied);
+        }
     }
-
-    if username == "admin" && password == "password" {
-        Some(LoginAction::Granted(LoginRole::Admin))
-    } else if username == "bob" && password == "password" {
-        Some(LoginAction::Granted(LoginRole::User))
-    } else {
-        Some(LoginAction::Denied)
-    }
+    None
 }
 
 #[cfg(test)]
