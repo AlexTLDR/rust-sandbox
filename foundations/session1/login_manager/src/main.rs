033d77a4ab9549815a2a8c8fc9fe4ab4b8e6a059
AlexTLDR <alex.badragan@protonmail.com>
2025-12-08 13:25:15 +0100

    feat(user_management): add user addition and deletion commands
    - Implemented `add_user` function to add new users.
    - Implemented `delete_user` function to remove existing users.
    - Updated CLI to support user listing, addition, and deletion.
    - Added `save_users` function to persist user data to JSON file.

--- foundations/session1/login_manager/src/main.rs
+++ foundations/session1/login_manager/src/main.rs
@@ -1,5 +1,5 @@
+use authentication::{LoginRole, User, get_users, save_users};
 use clap::{Parser, Subcommand};
-use authentication::get_users;
 
 #[derive(Parser)]
 #[command()]
@@ -12,6 +12,21 @@ struct Args {
 enum Commands {
     /// List all users.
     List,
+    /// Add a user
+    Add {
+        /// Username of the new user
+        username: String,
+        /// Password of the new user
+        password: String,
+        /// Optional - mark as an admin
+        #[arg(long)]
+        admin: Option<bool>,
+    },
+    /// Delete a user
+    Delete {
+        /// Username of the user to delete
+        username: String,
+    },
 }
 
 fn list_users() {
@@ -19,17 +34,47 @@ fn list_users() {
     println!("{:-<40}", "");
 
     let users = get_users();
-    users
-        .iter()
-        .for_each(|(_, user)| {
-            println!("{:<20}{:<20?}", user.username, user.role);
-        });
+    users.iter().for_each(|(_, user)| {
+        println!("{:<20}{:<20?}", user.username, user.role);
+    });
+}
+
+fn add_user(username: String, password: String, admin: bool) {
+    let mut users = get_users();
+    let role = if admin {
+        LoginRole::Admin
+    } else {
+        LoginRole::User
+    };
+    let user = User::new(&username, &password, role);
+    users.insert(username, user);
+    save_users(users);
+}
+
+fn delete_user(username: String) {
+    let mut users = get_users();
+    if users.contains_key(&username) {
+        users.remove(&username);
+        save_users(users);
+    } else {
+        println!("User '{}' not found.", username);
+    }
 }
 fn main() {
     let cli = Args::parse();
     match cli.command {
         Some(Commands::List) => {
             list_users();
+        }
+        Some(Commands::Add {
+            username,
+            password,
+            admin,
+        }) => {
+            add_user(username, password, admin.unwrap_or(false));
+        }
+        Some(Commands::Delete { username }) => {
+            delete_user(username);
         }
         None => {
             println!("Run with --help to see instructions.");
