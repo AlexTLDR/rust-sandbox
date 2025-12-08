d9a87a059292fc64242ef445f96ba79caf286916
AlexTLDR <alex.badragan@protonmail.com>
2025-12-08 12:12:32 +0100

    feat(auth): add password hashing and user data file
    * Implement password hashing using sha2 for secure storage
    * Update user creation to hash passwords before saving
    * Modify login function to hash input password for verification
    * Add users.json to store user credentials and roles

--- foundations/session1/authentication/src/lib.rs
+++ foundations/session1/authentication/src/lib.rs
@@ -1,7 +1,12 @@
-use std::collections::HashMap;
-use std::path::Path;
+use std::{collections::HashMap, path::Path};
 use serde::{Serialize, Deserialize };
 
+pub fn hash_password(password: &str) -> String {
+    use sha2::Digest;
+    let mut hasher = sha2::Sha256::new();
+    hasher.update(password);
+    format!("{:x}", hasher.finalize())
+}
 
 pub fn greet_user(name: &str) -> String {
     format!("Hello {}", name)
@@ -37,7 +42,7 @@ impl User {
     pub fn new(username: &str, password: &str, role: LoginRole ) -> User {
         User {
             username: username.to_lowercase(),
-            password: password.to_string(),
+            password: hash_password(password),
             role,
         }
     }
@@ -81,6 +86,7 @@ fn get_users() -> HashMap<String, User> {
 // }
 pub fn login(username: &str, password: &str) -> Option<LoginAction> {
     let username = username.to_lowercase();
+    let password = hash_password(password);
     let users= get_users();
     if let Some(user) = users.get(&username){
         if user.password == password {
