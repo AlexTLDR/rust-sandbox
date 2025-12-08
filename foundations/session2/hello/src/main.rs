2beeb73e74b28d4ba604f61f04d96fd8d4b471f4
AlexTLDR <alex.badragan@protonmail.com>
2025-12-08 17:50:11 +0100

    feat(threading): implement mathematical operations in threads
    * Added a new function `do_math` to perform calculations.
    * Updated thread spawning to execute `do_math` for a range of values.
    * Adjusted output to print results from each thread.

--- foundations/session2/hello/src/main.rs
+++ foundations/session2/hello/src/main.rs
@@ -2,14 +2,24 @@ fn hello_thread(n: u32) {
     println!("Hello from a thread {n}!");
 }
 
+fn do_math(i: u32) -> u32 {
+    let mut n = i + 1;
+    for _ in 0..10 {
+        n *= 2;
+    }
+    n
+}
+
 fn main() {
     println!("Hello from the main thread");
 
     let mut thread_handles = vec![];
-    for i in 0..5 {
-        let thread_handle = std::thread::spawn(move || hello_thread(i));
+    for i in 0..100 {
+        let thread_handle = std::thread::spawn(move || do_math(i));
         thread_handles.push(thread_handle);
     }
 
-    thread_handles.into_iter().for_each(|h| h.join().unwrap());
+    thread_handles
+        .into_iter()
+        .for_each(|h| println!("{}", h.join().unwrap()));
 }
