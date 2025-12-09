d19c7fb50dac08a9a2777aa3090e863a80f43800
AlexTLDR <alex.badragan@protonmail.com>
2025-12-09 15:00:22 +0100

    feat(channels1): add initial implementation of channels1 package
    * Create channels1 package with basic command handling
    * Implement multi-threading using mpsc channel for communication
    * Add functionality to send commands between threads

---
+++ foundations/session2/channels1/src/main.rs
@@ -0,0 +1,29 @@
+use std::sync::mpsc;
+
+enum Command {
+    SayHello,
+    Quit,
+}
+
+fn main() {
+    let (tx, rx) = mpsc::channel::<Command>();
+
+    let handle = std::thread::spawn(move || {
+        while let Ok(command) = rx.recv() {
+            match command {
+                Command::SayHello => println!("Hello from the thread!"),
+                Command::Quit => {
+                    println!("Quitting thread.");
+                    break;
+                }
+            }
+        }
+    });
+
+    for _ in 0..10 {
+        tx.send(Command::SayHello).unwrap();
+    }
+    println!("Sending quit");
+    tx.send(Command::Quit).unwrap();
+    handle.join().unwrap();
+}
