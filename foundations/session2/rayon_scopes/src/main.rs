b36507e4933afe2c9cbc7278ff5c921479e045b4
AlexTLDR <alex.badragan@protonmail.com>
2025-12-10 11:28:33 +0100

    feat(rayon): implement broadcast functionality in thread pool
    * Replace custom thread pool spawn with broadcast context
    * Maintain scoped thread spawning for additional threads
    * Update main thread message for clarity

--- foundations/session2/rayon_scopes/src/main.rs
+++ foundations/session2/rayon_scopes/src/main.rs
@@ -4,15 +4,21 @@ fn main() {
         .num_threads(4)
         .build()
         .unwrap();
-
-    pool.spawn(|| println!("Hello from a custom Rayon thread pool!"));
-
     pool.scope(|scope| {
-        for n in 0..20 {
-            scope.spawn(move |_| {
-                println!("Hello from scoped thread number {}", n);
-            })
-        }
+        scope.spawn_broadcast(|_scope, broadcast_context| {
+            println!("Hello from broadcast thread {}", broadcast_context.index())
+        })
     });
-    println!("Hello from the main thread");
+    /*
+       pool.spawn(|| println!("Hello from a custom Rayon thread pool!"));
+
+       pool.scope(|scope| {
+           for n in 0..20 {
+               scope.spawn(move |_| {
+                   println!("Hello from scoped thread number {}", n);
+               })
+           }
+       });
+       println!("Hello from the main thread");
+    */
 }
