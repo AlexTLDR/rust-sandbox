b31dffe031aa1a00c020bb32c74954da449d716b
AlexTLDR <alex.badragan@protonmail.com>
2025-12-10 10:45:50 +0100

    feat(tprio_thread_priority): add initial implementation of thread priority package
    * Introduced thread priority management with three priority levels: low, medium, and high.
    * Implemented atomic counters to track execution counts for each priority level.
    * Added main function to spawn threads and display their execution counts after 5 seconds.

---
+++ foundations/session2/tprio_thread_priority/src/main.rs
@@ -0,0 +1,49 @@
+use std::sync::atomic::AtomicI32;
+use thread_priority::*;
+
+static LOW_COUNT: AtomicI32 = AtomicI32::new(0);
+static MEDIUM_COUNT: AtomicI32 = AtomicI32::new(0);
+static HIGH_COUNT: AtomicI32 = AtomicI32::new(0);
+
+fn low_prio() {
+    set_current_thread_priority(ThreadPriority::Min).unwrap();
+    loop {
+        LOW_COUNT.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+        std::thread::yield_now();
+    }
+}
+
+fn medium_prio() {
+    //set_current_thread_priority(ThreadPriority::Max).unwrap();
+    loop {
+        MEDIUM_COUNT.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+        std::thread::yield_now();
+    }
+}
+
+fn high_prio() {
+    set_current_thread_priority(ThreadPriority::Max).unwrap();
+    loop {
+        HIGH_COUNT.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+        std::thread::yield_now();
+    }
+}
+
+fn main() {
+    std::thread::spawn(low_prio);
+    std::thread::spawn(medium_prio);
+    std::thread::spawn(high_prio);
+    std::thread::sleep(std::time::Duration::from_secs(5));
+    println!(
+        "Low: {}",
+        LOW_COUNT.load(std::sync::atomic::Ordering::Relaxed)
+    );
+    println!(
+        "Medium: {}",
+        MEDIUM_COUNT.load(std::sync::atomic::Ordering::Relaxed)
+    );
+    println!(
+        "High: {}",
+        HIGH_COUNT.load(std::sync::atomic::Ordering::Relaxed)
+    );
+}
