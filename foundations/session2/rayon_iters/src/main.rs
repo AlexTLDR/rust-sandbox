4e47b81577262b3e362fb39bfebbeca74eb630b2
AlexTLDR <alex.badragan@protonmail.com>
2025-12-10 11:08:52 +0100

    feat(rayon_iters): add initial implementation of rayon_iters package
    * Introduced rayon_iters package for parallel iteration.
    * Implemented prime number filtering and sorting using Rayon.
    * Updated Cargo.toml and Cargo.lock with new dependencies.

---
+++ foundations/session2/rayon_iters/src/main.rs
@@ -0,0 +1,21 @@
+use rayon::prelude::*;
+use std::time::Instant;
+
+fn is_prime(n: u32) -> bool {
+    (2..=n / 2).into_par_iter().all(|i| n % i != 0)
+}
+
+fn main() {
+    /*
+    let numbers: Vec<u64> = (0 .. 1_000_000).collect();
+    let sum = numbers.par_iter().sum::<u64>();
+    println!("The sum is: {}", sum);
+     */
+
+    let now = Instant::now();
+    let numbers: Vec<u32> = (0..100000).collect();
+    let mut primes: Vec<&u32> = numbers.par_iter().filter(|n| is_prime(**n)).collect();
+    primes.par_sort_unstable();
+    let elapsed = now.elapsed().as_secs_f32();
+    println!("Found {} primes in {:} seconds.", primes.len(), elapsed);
+}
