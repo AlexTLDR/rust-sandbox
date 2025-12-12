ba136b2dadda3edfd2a1e545344c94c07d4892a0
AlexTLDR <alex.badragan@protonmail.com>
2025-12-12 13:16:57 +0100

    feat(tokio_testing): add initial implementation of tokio_testing package
    * Create new tokio_testing package
    * Add basic add function and async double function
    * Include tests for both functions
    * Update Cargo.toml and Cargo.lock to include new package

---
+++ foundations/session3/tokio_testing/src/lib.rs
@@ -0,0 +1,23 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+async fn double(n: u32) -> u32 {
+    n * 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+    async fn test_double() {
+        assert_eq!(double(3).await, 6);
+    }
+}
