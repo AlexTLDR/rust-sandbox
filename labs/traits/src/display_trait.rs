eb5a6352f3b6937f99773a21bcfd0a45e6db4f40
AlexTLDR <alex.badragan@protonmail.com>
2025-12-29 19:13:55 +0100

    feat(traits): add print_barks function and string character count example in display_trait module

--- labs/traits/src/display_trait.rs
+++ labs/traits/src/display_trait.rs
@@ -12,10 +12,19 @@ impl fmt::Display for Cat {
     }
 }
 
+fn print_barks(input: String) {
+    println!("{} She barks at everybody!", input);
+}
+
 fn main() {
     let sushi = Cat {
         name: "Sushi".to_string(),
         age: 4,
     };
     println!("{}", sushi);
+    print_barks(sushi.to_string());
+    println!(
+        "Using .chars().count() to count the number of characters in the string: {}",
+        sushi.to_string().chars().count()
+    );
 }
