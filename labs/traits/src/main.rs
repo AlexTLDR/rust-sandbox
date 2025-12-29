32bb43b72d2adaa1d06ecfa383b848df1e7727aa
AlexTLDR <alex.badragan@protonmail.com>
2025-12-29 18:01:08 +0100

    feat(traits): add DogLike trait and implement for Animal and Parrot

--- labs/traits/src/main.rs
+++ labs/traits/src/main.rs
@@ -1,3 +1,33 @@
+struct Animal {
+    name: String,
+}
+
+struct Parrot {
+    name: String,
+}
+
+trait DogLike {
+    fn bark(&self) {
+        println!("Woof woof!");
+    }
+    fn run(&self) {
+        println!("Running...");
+    }
+}
+
+impl DogLike for Animal {}
+impl DogLike for Parrot {}
+
 fn main() {
-    println!("Hello, world!");
+    let rover = Animal {
+        name: "Rover".to_string(),
+    };
+    let polly = Parrot {
+        name: "Polly".to_string(),
+    };
+
+    rover.bark();
+    rover.run();
+    polly.bark();
+    polly.run();
 }
