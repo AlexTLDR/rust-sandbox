f5ac665bea0aa4882d754ac04f8cce65466a6d6a
AlexTLDR <alex.badragan@protonmail.com>
2025-12-30 16:24:22 +0100

    feat(traits): enhance complex_example with Debug, DisplayHealth trait, and health tracking

--- labs/traits/src/complex_example.rs
+++ labs/traits/src/complex_example.rs
@@ -1,21 +1,33 @@
+use std::fmt::Debug;
+
 struct Monster {
     health: i32,
 }
-struct Wizard {}
-struct Elf {}
-trait FightClose {
+#[derive(Debug)]
+struct Wizard {
+    health: i32,
+}
+#[derive(Debug)]
+struct Elf {
+    health: i32,
+}
+
+trait DisplayHealth {
+    fn health(&self) -> i32;
+}
+trait FightClose: Debug {
     fn attack_with_sword(&self, opponent: &mut Monster) {
         opponent.health -= 10;
         println!(
-            "Sword attack! Your opponent has {} health left.",
-            opponent.health
+            "Sword attack! Your opponent has {} health left. Your health is: {:?}",
+            opponent.health, self
         );
     }
     fn attack_with_hand(&self, opponent: &mut Monster) {
         opponent.health = -2;
         println!(
-            "Hand attack! Your opponent has {} health left.",
-            opponent.health
+            "Hand attack! Your opponent has {} health left. Your health is: {:?}",
+            opponent.health, self
         );
     }
 }
@@ -23,13 +35,13 @@ trait FightClose {
 impl FightClose for Wizard {}
 impl FightClose for Elf {}
 
-trait FightFromDistance {
+trait FightFromDistance: Debug {
     fn attack_with_bow(&self, opponent: &mut Monster, distance: u32) {
         if distance < 10 {
             opponent.health -= 10;
             println!(
-                "Bow attack! Your opponent has {} health left.",
-                opponent.health
+                "Bow attack! Your opponent has {} health left. Your health is: {:?}",
+                opponent.health, self
             );
         }
     }
@@ -37,8 +49,8 @@ trait FightFromDistance {
         if distance < 3 {
             opponent.health -= 4;
             println!(
-                "Rock attack! Your opponent has {} health left.",
-                opponent.health
+                "Rock attack! Your opponent has {} health left. Your health is: {:?}",
+                opponent.health, self
             );
         }
     }
@@ -46,8 +58,8 @@ trait FightFromDistance {
 impl FightFromDistance for Elf {}
 
 fn main() {
-    let gandalf = Wizard {};
-    let legolas = Elf {};
+    let gandalf = Wizard { health: 60 };
+    let legolas = Elf { health: 80 };
 
     let mut uruk_hai = Monster { health: 40 };
     gandalf.attack_with_sword(&mut uruk_hai);
