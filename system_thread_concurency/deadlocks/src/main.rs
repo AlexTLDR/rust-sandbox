247ff1f9bb90fa406d232e1da0efe14cf179d9a7
AlexTLDR <alex.badragan@protonmail.com>
2026-06-01 21:00:45 +0200

    feat: add deadlocks crate

---
+++ system_thread_concurency/deadlocks/src/main.rs
@@ -0,0 +1,23 @@
+use std::sync::Mutex;
+
+static MY_SHARED: Mutex<u32> = Mutex::new(3);
+
+fn poisoner() {
+    let mut lock = MY_SHARED.lock().unwrap();
+    *lock += 1;
+    panic!("The poisoner strikes");
+}
+
+fn main() {
+    let handle = std::thread::spawn(poisoner);
+    println!("Trying to return from the thread");
+    println!("{:?}", handle.join());
+
+    let lock = MY_SHARED.lock();
+    println!("{lock:?}");
+
+    let recovered_data = lock.unwrap_or_else(|poisoned| {
+        println!("Mutex was poisoned, recovering data...");
+        poisoned.into_inner()
+    });
+}
