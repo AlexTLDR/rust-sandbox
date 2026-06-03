cce4c3e484fa449e0f439c00bc6eb1b79eb3a6b2
AlexTLDR <alex.badragan@protonmail.com>
2026-06-03 23:59:55 +0200

    feat(async_await): extend async examples with `join_all` and `double` helper function

--- async_await/hello_async/src/main.rs
+++ async_await/hello_async/src/main.rs
@@ -1,9 +1,17 @@
 use futures::executor::block_on;
+use futures::future::join_all;
 use futures::join;
 
 async fn say_hello() {
     println!("Hello");
     join!(second_function(), say_goodbye());
+
+    let n = double(4).await;
+    println!("{n}");
+
+    let futures = vec![double(1), double(2), double(3)];
+    let results = join_all(futures).await;
+    println!("{results:?}");
 }
 
 async fn second_function() {
@@ -12,6 +20,10 @@ async fn second_function() {
 
 async fn say_goodbye() {
     println!("Goodbye!");
+}
+
+async fn double(n: u32) -> u32 {
+    n * 2
 }
 
 fn main() {
