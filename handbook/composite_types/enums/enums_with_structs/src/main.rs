0a6c46bbebf3bbc91dabb908772f5f3c3938c95a
AlexTLDR <alex.badragan@protonmail.com>
2026-06-24 21:49:00 +0200

    feat(enums): extend `enums_with_structs` example with additional Device fields and methods

--- handbook/composite_types/enums/enums_with_structs/src/main.rs
+++ handbook/composite_types/enums/enums_with_structs/src/main.rs
@@ -8,18 +8,28 @@ enum Status {
     Maintenance,
 }
 
+impl Status {
+    fn is_online(&self) -> bool {
+        matches!(self, Status::Online)
+    }
+}
+
 #[derive(Debug)]
 struct Device {
     id: u32,
     name: String,
     status: Status, // enum used as a field
+    ip_address: String,
+    location: String,
 }
 impl Device {
-    fn new(id: u32, name: String) -> Self {
+    fn new(id: u32, name: String, ip_address: String, location: String) -> Self {
         Self {
             id,
             name,
             status: Status::Offline, // default status
+            ip_address,
+            location,
         }
     }
     fn set_status(&mut self, new_status: Status) {
@@ -39,14 +49,35 @@ impl Device {
             ),
         }
     }
+    fn change_ip(&mut self, ip: String) {
+        self.ip_address = ip;
+    }
+    fn change_location(&mut self, location: String) {
+        self.location = location;
+    }
 }
 
 fn main() {
-    let mut router = Device::new(101, String::from("Main Router"));
+    let mut router = Device::new(
+        101,
+        String::from("Main Router"),
+        String::from("192.168.1.1"),
+        String::from("Server Room"),
+    );
     println!("{}", router.get_status_message());
     router.set_status(Status::Connecting { attempts: 1 });
     println!("{}", router.get_status_message());
     router.set_status(Online);
     println!("{}", router.get_status_message());
-    println!("Current device state: {:?}", router)
+    println!("Current device state: {:?}", router);
+    router.change_ip(String::from("192.168.0.1"));
+    router.change_location(String::from("GCP"));
+    println!(
+        "The new location of device named {} is {} and the new IP is {}",
+        router.name, router.location, router.ip_address
+    );
+    let node = Status::Online;
+    if node.is_online() {
+        println!("The node is online")
+    }
 }
