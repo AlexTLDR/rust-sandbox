c2ae1fa6fe1ab21b736adad31e7933d18b31418c
AlexTLDR <alex.badragan@protonmail.com>
2026-07-10 22:53:28 +0200

    feat(error_handling): add `file_reading_with_detailed_error_handling` example demonstrating error handling for reading files with detailed match cases

---
+++ handbook/exercises/error_handling/file_reading_with_detailed_error_handling/src/main.rs
@@ -0,0 +1,28 @@
+use std::fs;
+
+fn read_content(path: &str) -> Result<String, std::io::Error> {
+    std::fs::read_to_string(path)
+}
+
+fn main() {
+    println!("Calling read_content for an existing file");
+    fs::write("exists.txt", "This file exists and has some content");
+
+    match read_content("exists.txt") {
+        Ok(s) => println!("{}", s),
+        Err(e) => match e.kind() {
+            std::io::ErrorKind::NotFound => eprintln!("File not found"),
+            std::io::ErrorKind::PermissionDenied => eprintln!("Permission denied"),
+            _ => eprintln!("Another error: {}!", e),
+        },
+    }
+
+    match read_content("doesnot_exist.txt") {
+        Ok(s) => println!("This file doesn't exist, should not print this"),
+        Err(e) => match e.kind() {
+            std::io::ErrorKind::NotFound => eprintln!("File not found"),
+            std::io::ErrorKind::PermissionDenied => eprintln!("Permission denied"),
+            _ => eprintln!("Another error: {}!", e),
+        },
+    }
+}
